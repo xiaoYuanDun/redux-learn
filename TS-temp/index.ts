da35436a2f903f1adf4ff854a49703453f0dfdb4
xiaoYuanDun <649173212@qq.com>
2021-03-09 21:32:48 +0800

    fix:调整一个小类型

--- TS-temp/index.ts
+++ TS-temp/index.ts
@@ -1,37 +1,37 @@
-type State = any
+type State = any;
 type sagaCall = {
-    call: Function
-    put: Function
-}
+  call: Function;
+  put: Function;
+};
 
 const model = {
-    reducer: {
-        extraAction(state: State, payload: { id: string, age: number }){ 
-            console.log(111)
-            return state
-        },
-        add(state: State){ 
-            console.log(222)
-            return state
-        },
-        insert(state: State, userData: any) {
-            console.log('insert user data',userData)
-        }
+  reducer: {
+    extraAction(state: State, payload: { id: string; age: number }) {
+      console.log(111);
+      return state;
     },
-    effects: {
-        *fetchData({call}: sagaCall){
-            console.log('fetch data')
-        },
-        *fetchUserData(id: string, {call, put}: sagaCall){
-            console.log('fetch user data')
-            console.log('userId', id)
-            put('userData')
-        }
-    }
-}
+    add(state: State) {
+      console.log(222);
+      return state;
+    },
+    insert(state: State, userData: any) {
+      console.log('insert user data', userData);
+    },
+  },
+  effects: {
+    *fetchData({ call }: sagaCall) {
+      console.log('fetch data');
+    },
+    *fetchUserData(id: string, { call, put }: sagaCall) {
+      console.log('fetch user data');
+      console.log('userId', id);
+      put('userData');
+    },
+  },
+};
 
 /**
- * 整理需求，总结函数类型: 
+ * 整理需求，总结函数类型:
  * 1. reducer 函数有可能不带 payload, 只有一个默认参数 state, 最多2个参数
  * 2. reducer state 参数总是位于第一个
  * 3. effects 函数有可能不带 payload, 只有一个默认 sagaCall 参数, 最多2个参数
@@ -40,75 +40,74 @@ const model = {
 
 // 思路1, 全部使用 函数重载进行
 // 得到 Model 类型
-type Model = typeof model
+type Model = typeof model;
 
 // 得到所有 reducer, effect 包含的 function, 用于后面的类型提示
-type AllReducer = Model['reducer']
-type AllFunction =  AllReducer & Model['effects']
+type AllReducer = Model['reducer'];
+type AllFunction = AllReducer & Model['effects'];
 
 type GetOverload<T, K extends keyof T = keyof T> = {
-    [k in K]: (type: k) => any
-}
+  [k in K]: (type: k) => any;
+};
 
-type Values<T, K extends keyof T = keyof T> = T[K]
+type Values<T, K extends keyof T = keyof T> = T[K];
 
-type AllTypedObj = GetOverload<AllFunction>
+type AllTypedObj = GetOverload<AllFunction>;
 
-type Res1 = Values<AllTypedObj>
+type Res1 = Values<AllTypedObj>;
 
-type Jiagong1<T> = T extends any ? (temp: T) => void : never
-type r1 = Jiagong1<Res1>
+type Jiagong1<T> = T extends any ? (temp: T) => void : never;
+type r1 = Jiagong1<Res1>;
 
-type Jiagong2<T> = [T] extends [(k: infer R) => void] ? R : never
-type r2 = Jiagong2<r1>
+type Jiagong2<T> = [T] extends [(k: infer R) => void] ? R : never;
+type r2 = Jiagong2<r1>;
 
-const commit: r2 = () => {}
+const commit: r2 = () => {};
 
 // commit('extraAction')
 
-
 // 思路2, 构造对应格式进行
-type P1<T, K extends keyof T = keyof T> = T extends any ? T[K] : never
-type ConReducer<T> = T extends ((type: infer R1, payload: infer R2) => void) 
-  ? ((type: R1, payload: R2) => void)  
-  : T extends ((type: infer R1) => void)
-    ? ((type: R1) => void) : never
+type P1<T, K extends keyof T = keyof T> = T extends any ? T[K] : never;
+type ConReducer<T> = T extends (type: infer R1, payload: infer R2) => void
+  ? (type: R1, payload: R2) => void
+  : T extends (type: infer R1) => void
+  ? (type: R1) => void
+  : never;
 
-type reducerOverload = P1<AllReducer>
+type reducerOverload = P1<AllReducer>;
+// 1
 
-
-
-// use Lookup<T, K> instead of T[K] in cases where the compiler 
+// use Lookup<T, K> instead of T[K] in cases where the compiler
 //  cannot verify that K is a key of T
 // type Lookup<T, K> = K extends keyof T ? T[K] : never;
 
 // type getKeyProp<T extends keyEnum> = Lookup<UnionToIntersection<keyPropObj[T]>, 'prop'>;
- 
 
-type TestType = ((key: 'a') => any) | ((key: 'b') => any)
+type TestType = ((key: 'a') => any) | ((key: 'b') => any);
 
-type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never;
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
+  k: infer I
+) => void
+  ? I
+  : never;
 
+type T_T = UnionToIntersection<TestType>;
 
-type T_T = UnionToIntersection<TestType>
+type SectionType = {};
 
-type SectionType = {} 
-
-type UToI_1<T> = T extends any ? (k: T) => void : never
-type T1 = UToI_1<TestType>
+type UToI_1<T> = T extends any ? (k: T) => void : never;
+type T1 = UToI_1<TestType>;
 
 // 裸类型
 // 这是类型变会变为 分布式条件类型, 所以是 | 的关系
-type UToI_2<T> = T extends (k: infer I) => void ? I : never
-type T2 = UToI_2<T1>
-
+type UToI_2<T> = T extends (k: infer I) => void ? I : never;
+type T2 = UToI_2<T1>;
 
 // 注意看这里的区别, 只是给泛型 T 外部的加上 [], 这是 T 的引用不在是裸类型, 不会变为 分布式条件类型, 所以关系是 &
-type UToI_2_2<T> = [T] extends [(k: infer I) => void] ? I : never
-type T2_2 = UToI_2_2<T1>
+type UToI_2_2<T> = [T] extends [(k: infer I) => void] ? I : never;
+type T2_2 = UToI_2_2<T1>;
 
-
-// 
+//
 /**
  * 分布式条件形成的条件
  * 裸类型
@@ -116,50 +115,44 @@ type T2_2 = UToI_2_2<T1>
  * https://www.bilibili.com/video/av754591185?p=1
  * https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-8.html#distributive-conditional-types
  * https://www.tslang.cn/docs/release-notes/typescript-2.8.html
- * 
+ *
  * TODO
  * https://juejin.cn/post/6844904066179579918
  * https://segmentfault.com/a/1190000018514540?utm_source=tag-newest
  * https://github.com/Microsoft/TypeScript/pull/21496
  */
 
+type getInterSection<T> = [T] extends [(key: infer R) => any] ? R : never;
+type T3 = getInterSection<TestType>;
 
-
-type getInterSection<T> = [T] extends [(key: infer R) => any] ? R : never
-type T3 = getInterSection<TestType>
-
-type Obj = { name: string, age: number }
-type A = keyof Obj
-
-
-
+type Obj = { name: string; age: number };
+type A = keyof Obj;
 
 // -------------------------------------------------------------------
 // 普通 Diff 只能用于简单类型过滤, 想过滤对象的属性diff, 可使用如下增强类型
-type Test1 = {name: string, age: number}
-type Test2 = {name: string, sex: string }
+type Test1 = { name: string; age: number };
+type Test2 = { name: string; sex: string };
 
-type DiffKey<T, U> = T extends U ? never : T
+type DiffKey<T, U> = T extends U ? never : T;
 
-type Diff<T, U> = DiffKey<keyof T, keyof U> | DiffKey<keyof U, keyof T>
+type Diff<T, U> = DiffKey<keyof T, keyof U> | DiffKey<keyof U, keyof T>;
 
 type DiffValue<T, U, Z extends keyof (T & U)> = {
-  [K in Z]: (T & U)[K]
-}
+  [K in Z]: (T & U)[K];
+};
 
-type DiffObjAttr<T, U> = DiffValue<T, U, Diff<T, U>>
+type DiffObjAttr<T, U> = DiffValue<T, U, Diff<T, U>>;
 
 // 先得到不同的 key
-type TempDiffKey = Diff<Test1, Test2>
+type TempDiffKey = Diff<Test1, Test2>;
 
 // 从两个类型的交叉类型中取出对应的 key
-type TempDiffVal = DiffValue<Test1, Test2, TempDiffKey>
+type TempDiffVal = DiffValue<Test1, Test2, TempDiffKey>;
 
 // 最终写法
-type ObjDiffAttr = DiffObjAttr<Test1, Test2>
+type ObjDiffAttr = DiffObjAttr<Test1, Test2>;
 
-
-type Bar<T> = T extends { a: (x: infer U) => void, b: (x: infer U) => void } ? U : never;
-type T21 = Bar<{ a: (x: string) => void, b: (x: number) => void }>;
-
-
+type Bar<T> = T extends { a: (x: infer U) => void; b: (x: infer U) => void }
+  ? U
+  : never;
+type T21 = Bar<{ a: (x: string) => void; b: (x: number) => void }>;
